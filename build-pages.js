b8df29d54ee660266cadddca22fe39c452b14450
Lion d'Oc <mathias.szanto@gmail.com>
2026-08-20 10:02:43 +0200

    Adoucit la couleur du bandeau d'alerte

--- build-pages.js
+++ build-pages.js
@@ -17,7 +17,7 @@ function header(root, isHome) {
   <title>{{TITLE}} | Espace Balnéo Gruissan</title>
   <meta name="description" content="{{DESC}}">
   <link rel="stylesheet" href="${p}/assets/vendor/fontawesome/css/all.min.css">
-  <link rel="stylesheet" href="${p}/css/styles.css?v=20260819-29">
+  <link rel="stylesheet" href="${p}/css/styles.css?v=20260820-1">
 </head>
 <body class="${isHome ? 'is-home' : ''}">
   <header class="site-header">
